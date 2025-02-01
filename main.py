c9a0d15879e0586e45b0ce7c8f341847761d25b5
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2025-02-01 13:47:49 +0200

    LIST AND DICTIONARY

---
+++ main.py
@@ -0,0 +1,26 @@
+foods = ["rice", "beans", "yam", "potatoes", "noodles", "cassava"]
+print(foods[3])
+foods.append("fufu")
+print(foods)
+foods.pop()
+print(foods)
+foods.sort()
+print(foods)
+# foods.clear()
+print (foods)
+foods.reverse()
+foods.remove("yam")
+print(foods)
+
+
+#dictionary
+student1 = {
+    "name":"Vivian",
+    "age" : 14,
+    "hobby" : "Reading Stories and Coding"
+}
+print(student1 ["hobby"])
+student1["name"] = "Nwasinachi"
+print(student1)
+student1.clear()
+print(student1)
